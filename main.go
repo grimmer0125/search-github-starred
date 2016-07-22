fccef2302476e1d5cde04796ec54916497de92ce
Grimmer Kang <grimmer0125@gmail.com>
2016-07-23 02:31:57 +0800

    Delete some commented lines.

--- main.go
+++ main.go
@@ -1,30 +1,10 @@
-// package main
-//
-// import (
-// 	"net/http"
-// 	"os"
-//
-// 	"github.com/gin-gonic/gin"
-// )
-//
-// func main() {
-//
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = "5000"
-// 	}
-//
 // 	router := gin.New()
 // 	router.Use(gin.Logger())
 // 	router.LoadHTMLGlob("templates/*.tmpl.html")
-// 	router.Static("/static", "static")
-//
+
 // 	router.GET("/", func(c *gin.Context) {
 // 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 // 	})
-//
-// 	router.Run(":" + port)
-// }
 
 package main
 
